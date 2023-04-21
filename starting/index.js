6b5894dec1d5ce73c0dfbc03cb51b4d1df1a4dec
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2023-04-21 20:48:20 +0300

    Update index.js

--- starting/index.js
+++ starting/index.js
@@ -1,5 +1,5 @@
 const { TelegramBot, Collection, Button } = require("telegramsjs");
-const { CreateStorage } = require('database-sempai');
+//const { CreateStorage } = require('database-sempai');
 const { loaderCmd, loaderEvent } = require("./src/index.js");
 
 const bot = new TelegramBot('', {
@@ -8,13 +8,13 @@ const bot = new TelegramBot('', {
 
 bot.commands = new Collection();
 bot.exudates = new Collection();
-bot.db = new CreateStorage({
+/*bot.db = new CreateStorage({
   path: "database-sempai",
   table: ["click", "command_blacklist", "cooldown", "currency", "global", "language", "main", "quest",  "shop", "supergroup",  "system", "timers", "username"]
 })
 bot.random = function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+}*/
 
 loaderCmd(bot, "./cmd/");
 loaderEvent(bot, "./event/");
