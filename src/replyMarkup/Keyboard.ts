2664ad94b4d214ec59b44cf1792ff3daf0fe51da
Sempai-07 <mausm471@gmail.com>
2023-07-03 17:54:28 +0300

    add types

--- src/replyMarkup/Keyboard.ts
+++ src/replyMarkup/Keyboard.ts
@@ -1,21 +1,21 @@
+import { type InlineKeyboardButton } from '@grammyjs/types';
+
+type InlineKeyboard = {
+  resize_keyboard?: boolean;
+  one_time_keyboard: boolean;
+  selective: boolean;
+  keyboard: InlineKeyboardButton[][]
+}
+
 export class Keyboard {
-  private markups: any[][];
+  private keyboard: InlineKeyboardButton[][] = [];
   private inline: boolean;
   private resize: boolean;
   private oneTime: boolean;
   private selective: boolean;
 
-  /**
-   * Creates a new Keyboard object.
-   * @param {Array} markups - A 2-dimensional array of Button objects representing the markups on the keyboard.
-   * @param {Object} [options={}] - Additional options for the keyboard.
-   * @param {boolean} [options.inline=false] - Whether the keyboard is an inline keyboard.
-   * @param {boolean} [options.resize=false] - Whether the keyboard should be resized to fit the user's screen.
-   * @param {boolean} [options.oneTime=false] - Whether the keyboard should disappear after the user presses a markup.
-   * @param {boolean} [options.selective=false] - Whether the keyboard should be shown only to specific users.
-   */
   constructor(
-    markups: any[][],
+    markups: InlineKeyboardButton[][] = [],
     options: {
       inline?: boolean;
       resize?: boolean;
@@ -23,7 +23,7 @@ export class Keyboard {
       selective?: boolean;
     } = {}
   ) {
-    this.markups = markups ?? [];
+    this.keyboard = markups;
     this.inline = options.inline ?? false;
     this.resize = options.resize ?? false;
     this.oneTime = options.oneTime ?? false;
@@ -33,14 +33,14 @@ export class Keyboard {
 
   /**
    * Returns the keyboard object in the format expected by Telegram Bot API.
-   * @returns {object} Returns the keyboard object in the format expected by Telegram Bot API.
+   * @returns {InlineKeyboard} Returns the keyboard object in the format expected by Telegram Bot API.
    */
-  toJSON(): object {
-    const keyboard = {
-      keyboard: this.markups,
+  toJSON(): InlineKeyboard {
+    let keyboard: InlineKeyboard = {
       resize_keyboard: this.resize,
       one_time_keyboard: this.oneTime,
       selective: this.selective,
+      keyboard: this.keyboard
     };
     return keyboard;
   }
@@ -50,7 +50,7 @@ export class Keyboard {
    * @param {boolean} inline - Whether the keyboard should be displayed inline with the message.
    * @returns {Keyboard} Returns the updated Keyboard object.
    */
-  setInline(inline: boolean) {
+  setInline(inline: boolean): Keyboard {
     this.inline = inline;
     return this;
   }
@@ -60,7 +60,7 @@ export class Keyboard {
    * @param {boolean} resize - Whether the keyboard should be resized to fit the user's screen.
    * @returns {Keyboard} Returns the current `Keyboard` object for chaining.
    */
-  setResize(resize: boolean) {
+  setResize(resize: boolean): Keyboard {
     this.resize = resize;
     return this;
   }
@@ -70,7 +70,7 @@ export class Keyboard {
    * @param {boolean} oneTime - Whether the keyboard should disappear after the user presses a markup.
    * @returns {Keyboard} Returns the Keyboard object.
    */
-  setOneTime(oneTime: boolean) {
+  setOneTime(oneTime: boolean): Keyboard {
     this.oneTime = oneTime;
     return this;
   }
@@ -80,25 +80,29 @@ export class Keyboard {
    * @param {boolean} selective - Whether the keyboard should be shown only to specific users.
    * @returns {Keyboard} Returns the `Keyboard` object with the `selective` option set.
    */
-  setSelective(selective: boolean) {
+  setSelective(selective: boolean): Keyboard {
     this.selective = selective;
     return this;
   }
 
   /**
    * Adds markups to the keyboard markups array.
-   * @param  {...any} markupRows - One or more markup rows to add to the keyboard.
+   * @param  {...InlineKeyboardButton[]} markupRows - One or more markup rows to add to the keyboard.
    * @param {boolean} [defaults] - Specifies whether to use default values for the markups.
    * @returns {Keyboard} Returns the updated `Keyboard` object.
    */
-  addKeyboard(markupRows: any[], defaults = false) {
+  addKeyboard(
+    markupRows: (InlineKeyboardButton | InlineKeyboardButton[])[],
+    defaults: boolean = false
+  ): Keyboard {
     for (let i = 0; i < markupRows.length; i++) {
-      if (defaults) this.markups = markupRows;
-      else if (defaults === false) {
-        const row = Array.isArray(markupRows[i])
-          ? markupRows[i]
-          : [markupRows[i]];
-        this.markups.push(row);
+      if (defaults) {
+        this.keyboard = markupRows as InlineKeyboardButton[][];
+      } else if (defaults === false) {
+        const row: InlineKeyboardButton[] = Array.isArray(markupRows[i])
+          ? markupRows[i] as InlineKeyboardButton[]
+          : [markupRows[i] as InlineKeyboardButton];
+        this.keyboard.push(row);
       }
     }
     return this;
