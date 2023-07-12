5e759471883e853cb64ff907636b437ce29c30cc
Sempai-07 <mausm471@gmail.com>
2023-07-12 12:49:03 +0300

    delete QueryString

--- src/constanta/QueryString.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Represents different query string types.
- */
-export const QueryString = {
-  Multipart: "multipart/form-data",
-  ApplicationJSON: "application/json",
-  ApplicationUrlencoded: "application/x-www-form-urlencoded",
-};
