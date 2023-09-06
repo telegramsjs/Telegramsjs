8f6dd111ec6ad258e75d4ab0f02f8ab548642a3f
Sempai-07 <suhozadvova7@gmail.com>
2023-09-06 15:49:57 +0300

    update error-api

--- src/error.ts
+++ src/error.ts
@@ -3,18 +3,17 @@ import { ResponseParameters } from "@telegram.ts/types";
  * Represents an error that occurs when interacting with the Telegram Bot API.
  */
 class TelegramApiError extends Error {
-  code: number;
   ok: boolean;
+  code: number;
   method: string;
-  parameters?: ResponseParameters | object;
-  params?: object;
-
+  parameters?: ResponseParameters;
   /**
    * Constructs a new TelegramApiError object.
+   *
    * @param {object | string} error - The error object or error message.
    * @param {string} method - The method that caused the error.
+   * @param {object} [params] - Additional parameters related to the error.
    */
-
   constructor(
     error: {
       error_code?: number;
@@ -25,22 +24,22 @@ class TelegramApiError extends Error {
     method: string,
     params?: object,
   ) {
-    let message = { description: "unknown error" };
+    let message = { description: "Unknown error" };
 
     if (typeof error === "string") {
       message.description = error;
     }
+
     if (error.description) {
       const errorDescription = error.description.split(":")[1];
       if (errorDescription) {
-        message.description = errorDescription.trimStart().toLocaleLowerCase();
+        message.description = errorDescription.trimStart().toLowerCase();
       } else {
         message.description = error.description;
       }
     }
 
     super(message.description);
-
     this.name = `TelegramApiError[${error.error_code ?? 404}]`;
     this.parameters = error.parameters ?? params;
     this.method = method;
