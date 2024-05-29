f75cf6bd5825990fb2b1d1907ec2a70f0ab30633
Sempai-07 <mausm471@gmail.com>
2024-05-29 21:54:26 +0300

    Updated and fixes

--- src/api.ts
+++ src/api.ts
@@ -1000,17 +1000,6 @@ class Api extends ApiRequest {
     })
   }
 
-  /** Refunds a successful payment in Telegram Stars. Returns True on success */
-  async refundStarPayment(userId: number, telegramPaymentChargeId: string) {
-    return await this.request<MethodsReturnType["refundStarPayment"]>(
-      "refundStarPayment",
-      {
-        user_id: userId,
-        telegram_payment_charge_id: telegramPaymentChargeId,
-      },
-    );
-  }
-
   /** Informs a user that some of the Telegram Passport elements they provided contains errors. The user will not be able to re-submit their Passport to you until the errors are fixed (the contents of the field for which you returned the error must change). Returns True on success.
 
   Use this if the data submitted by the user doesn't satisfy the standards your service requires for any reason. For example, if a birthday date seems invalid, a submitted document is blurry, a scan shows evidence of tampering, etc. Supply some details in the error message to make sure the user knows how to correct the issues. */
