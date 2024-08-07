1c2e5d360a63c2f81f4d26dc08a40209954558eb
Sempai-07 <118384270+Sempai-07@users.noreply.github.com>
2024-08-07 09:40:35 +0300

    Delete dist directory

--- dist/src/util/Utils.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Checks if a given string follows camelCase naming convention, is completely lowercase, or contains only digits.
- * @param str - The string to check.
- * @returns Returns `true` if the string is in camelCase format or completely lowercase, otherwise `false`.
- */
-declare function isCamelCase(str: string): boolean;
-export { isCamelCase };
-//# sourceMappingURL=Utils.d.ts.map
