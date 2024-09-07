93a3fe6cff1df6651d8075bdaa3ba3af41e1ccda
Sempai-07 <mausm471@gmail.com>
2024-09-07 22:18:54 +0300

    Fix(ChatInviteLink): propetry subscriptionPeriod and subscriptionPrice

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4462,6 +4462,10 @@ export declare class ChatInviteLink extends Base {
    * Number of pending join requests created using this link
    */
   requestsCount?: number;
+  /** The number of seconds the subscription will be active for before the next payment */
+  subscriptionPeriod?: number;
+  /** The amount of Telegram Stars a user must pay initially and after each subsequent subscription period to be a member of the chat using the link */
+  subscriptionPrice?: number;
   /**
    * Return the timestamp link will expire or has been expired, in milliseconds
    */
